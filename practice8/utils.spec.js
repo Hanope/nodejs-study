010617f03f68d186f6a4772491ebc6da4c12c813
Hanope <heemanghan@gmail.com>
2017-11-06 22:20:53 +0900

    Mocha를 활용한 Test code 작성, 테스트 실행

--- practice8/utils.spec.js
+++ practice8/utils.spec.js
@@ -1,7 +1,16 @@
-var utils = require('/.utils');
+// xxx.spec.js 파일들은 거의 테스트 코드이다.
+const utils = require('./utils');
+// const assert = require('assert');
 
-describe(describe('utils.js'), () => {
-    if('문자열의 첫분째 문자를 대문자로 변환한다ㅏ.' () => {
+// should를 사용함으로써 Test code의 가독성을 높임.
+const should = require('should');
 
+// describe: 테스트 환경 구현
+describe('utils.js모듈의 capitalize() 함수는', () => {
+    // 테스트 케이스 구현
+    it('문자열의 첫번째 문자를 대문자로 변환한다.', () => {
+        const result = utils.capitalize('hello');
+        // assert.equal(result, 'Hello');
+        result.should.be.equal('Hello');
     })
-});
+})
