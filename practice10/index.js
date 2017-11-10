3542d2fd8e5d6b2e5814eafe03615b4c3c10efe6
Hanope <heemanghan@gmail.com>
2017-11-10 21:16:38 +0900

    DELETE 기능 구현 및 Test code 작성

--- practice10/index.js
+++ practice10/index.js
@@ -25,8 +25,19 @@ app.get('/users', function(req, res) {
 
 app.get('/users/:id', function(req, res) {
     const id = parseInt(req.params.id, 10);
+    if(Number.isNaN(id)) return res.status(400).end();
+
     const user = users.filter((user) => user.id === id)[0];
+    if(!user) return res.status(404).end();
     res.json(user);
+});
+
+app.delete('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    if(Number.isNaN(id)) return res.status(400).end();
+    
+    users = users.filter(user => user.id !== id);
+    res.status(204).end();
 });
 
 app.post('/users', function(req, res) {
