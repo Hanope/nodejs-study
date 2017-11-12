61320332c4b4bca476123e84913a4f0f763d0b83
Hanope <heemanghan@gmail.com>
2017-11-12 20:16:14 +0900

    Test환경과 Server 실행환경 분리

--- practice10/index.js
+++ practice10/index.js
@@ -4,15 +4,14 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var user = require('./api/user')
 
-app.use(morgan('dev'));
+// Test환경일 경우 log 찍지 않음
+if(process.env.NODE_ENV !== 'test') {
+    app.use(morgan('dev'));
+}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 // 라우터 설정
 app.use('/users', user);
-
-app.listen(3000, function() {
-    console.log('Example app listening on port 3000!');
-});
 
 module.exports = app;
